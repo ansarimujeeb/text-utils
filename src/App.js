8c58603e084cbef4e811361bc5c6207dd60bfb23
Mujeeb <ansarimujeeb06@gmail.con>
2022-11-13 22:27:34 +0530

    toggle enabme dark mode with alert

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
-//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react'
+import Alert from './components/Alert';
 
 function App() {
+    const [ mode, setMode] = useState('light');
+    const [alert, setAlert] = useState(null);
+
+    const showAlert = (message, type) => {
+        setAlert({
+            msg: message,
+            type : type
+        })
+        setTimeout(() => {
+            setAlert(null)
+        }, 1500 )
+    }
+    const toggleMode = () => {
+        if(mode === 'dark') {
+            setMode('light');
+            document.body.style.backgroundColor = 'white';
+            showAlert('Light mode enable','success');
+        } else {
+            setMode('dark');
+            document.body.style.backgroundColor = '#343a40';
+            showAlert('Dark mode enable','success');
+        }
+    }
     return (
     <>
-        <Navbar title="TextUtils3" />
+        <Navbar title="TextUtils3" mode={mode} toggleMode={toggleMode}/>
         <div className="container my-3">
-            <TextForm headingText="Enter the text below"/>
-            {/* <About /> */}
+        <Alert alert={alert}/>
+            <TextForm headingText="Enter the text below" mode={mode} showAlert={showAlert}/>
         </div>
     </>    
     );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,11 +5,12 @@ export default function TextForm(props)  {
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert('Converted to Uppercase','success');
     }
     const handleLoClick = () => {
-        console.log('btn click');
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert('Converted to Lowercase','success');
     }
     const handleOnChange = (event) => {
         setText(event.target.value);
@@ -19,24 +20,32 @@ export default function TextForm(props)  {
         let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert('Copy to clipboard','success');
     }
     const handleSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert('Remove extra space','success');
+    }
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+        props.showAlert('text Clear','success');
     }
     return (
     <>
-        <div className='container'>
+        <div className='container' style={ { color : props.mode === 'dark' ? 'white' : '#343a40' }}>
             <h1>{props.headingText }</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="6" placeholder='Enter text here'></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="6" style={ { backgroundColor : props.mode === 'dark' ? '#343a40' : 'white', color : props.mode === 'dark' ? 'white' : '#343a40' }}></textarea>
             </div>
             <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to Upper Case</button>
             <button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert to Lower Case</button>
+            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
             <button className='btn btn-primary mx-1' onClick={handleCopy}>Copy Text</button>
             <button className='btn btn-primary mx-1' onClick={handleSpaces}>Remove Spaces</button>
         </div>
-        <div className='container my-3'>
+        <div className='container my-3' style={ { color : props.mode === 'dark' ? 'white' : '#343a40' }}>
             <h1> Your Text Summary</h1>
             <p> {text.split(" ").length} word and {text.length} characters</p>
             <p> { 0.008 * text.split(" ").length } Minutes to read</p>
